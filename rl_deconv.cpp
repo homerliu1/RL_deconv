783139375c972d39afeaf6e3f3a5d88e326f7220
homerliu1 <liu-xh@live.jp>
2022-12-01 14:38:17 +0900

    genPSF(41); iterations 50

--- rl_deconv.cpp
+++ rl_deconv.cpp
@@ -65,30 +65,11 @@ Mat genPSF(int sz) {
 	return psf;
 }
 
-int main( int argc, const char** argv )
+void show3Imgs(Mat org_flt, Mat blurred_flt, Mat estimation)
 {
-	if (argc != 3) {
-		cout << "Usage: " << argv[0] << " image iterations" << "\n";
-		return -1;
-	}
-	int iterations = atoi(argv[2]);
-	Mat original_image = imread(argv[1], 0);
-	Size imsz = original_image.size();
-
-	// From here on, use floats	// Convert original_image to float
-	int type_f = CV_32FC1;
-	Mat org_flt;
-	original_image.convertTo(org_flt, type_f, 1.f/255);
+	Size imsz = org_flt.size();
 	
-	Mat psf = genPSF(35);
-
-	// Blur the float_image with the psf.
-	Mat blurred_flt = org_flt.clone();
-	filter2D(org_flt, blurred_flt, -1, psf);
-
-	Mat estimation = rl_deconvol(blurred_flt, psf, iterations);
-
-	Mat est_view = Mat(imsz.height, imsz.width*3, type_f);
+	Mat est_view = Mat(imsz.height, imsz.width*3, CV_32FC1);
 	Rect rc_lft = Rect(0, 				0, imsz.width, imsz.height);
 	Rect rc_mid = Rect(imsz.width,		0, imsz.width, imsz.height);
 	Rect rc_rht = Rect(imsz.width*2,	0, imsz.width, imsz.height);
@@ -99,9 +80,31 @@ int main( int argc, const char** argv )
 
 	namedWindow("Estimation", 0);	
 	imshow("Estimation", est_view);
-	waitKey(10); //cause update 
+	waitKey(20); //cause update 
 	setWindowProperty("Estimation", WND_PROP_FULLSCREEN, 1); 
-	waitKey(0); 
+}
 
-	return 0;
+int main( int argc, const char** argv )
+{
+	if (argc != 3) {
+		cout << "Usage: " << argv[0] << " image iterations" << "\n";
+		return -1;
+	}
+	int iterations = atoi(argv[2]);
+	Mat original_image = imread(argv[1], 0);
+
+	Mat org_flt;
+	original_image.convertTo(org_flt, CV_32FC1, 1.f/255);
+	
+	Mat psf = genPSF(41);
+
+	// Blur the float_image with the psf.
+	Mat blurred_flt = org_flt.clone();
+	filter2D(org_flt, blurred_flt, -1, psf);
+
+	Mat estimation = rl_deconvol(blurred_flt, psf, iterations);
+
+	show3Imgs(org_flt, blurred_flt, estimation);
+
+	return waitKey(0); 
 }
